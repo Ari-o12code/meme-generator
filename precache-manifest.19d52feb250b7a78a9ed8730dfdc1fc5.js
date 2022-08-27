691cd6bea06b3a01cff7f15c0e22bf6ff474e8b7
Quadri Alameen <quadrameen@gmail.com>
2022-08-27 18:28:32 +0100

    Updates

---
+++ precache-manifest.19d52feb250b7a78a9ed8730dfdc1fc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ef8329048d615a70d6f",
+    "url": "/meme-generator/static/css/main.02d13852.chunk.css"
+  },
+  {
+    "revision": "9ef8329048d615a70d6f",
+    "url": "/meme-generator/static/js/main.9ef83290.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/meme-generator/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "83720320f05a1dfbb1e4",
+    "url": "/meme-generator/static/css/2.1380bacc.chunk.css"
+  },
+  {
+    "revision": "83720320f05a1dfbb1e4",
+    "url": "/meme-generator/static/js/2.83720320.chunk.js"
+  },
+  {
+    "revision": "176d6309747887ca51c6",
+    "url": "/meme-generator/static/js/runtime~main.176d6309.js"
+  },
+  {
+    "revision": "b35a64ea0e5a897e4f53a78294e2b383",
+    "url": "/meme-generator/index.html"
+  }
+];
